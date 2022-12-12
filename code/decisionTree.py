d54c39ca65a8961f436ab03d164a9288eb456209
Kathryn <kpanger88@gmail.com>
2022-12-12 02:10:36 -0600

    continuous surprise, p-value v1 complete

--- code/decisionTree.py
+++ code/decisionTree.py
@@ -2,7 +2,9 @@ from cleanDataFrame import CleanDataFrame
 from typing import List
 import pyspark
 from pyspark.sql import functions as F
+from pyspark.sql.types import FloatType
 from scipy import stats
+
 
 class DecisionTree():
     def __init__(self, data: pyspark.sql.DataFrame):
@@ -23,22 +25,43 @@ class DecisionTree():
         for col in data.columns:
             if col in varlist:
                 #get the sample mean
-                sampleMean = data.select(col).agg(F.mean(col)).collect()[0][0]
-                sampleStdDev = data.select(col).agg(F.stddev(col)).collect()[0][0]
+                sampleMean = float(data.select(col).agg(F.mean(col)).collect()[0][0])
+                sampleStdDev = float(data.select(col).agg(F.stddev(col)).collect()[0][0])
                 sampleN = self.n
-                print(sampleStdDev)
-
-                standardError = sampleStdDev/sampleN**(1/2)
-                degreesOfFreedom = n-1
+                # standardError = sampleStdDev/sampleN**(1/2)
+                degreesOfFreedom = sampleN-1
                 #create a pyspark function to get probability of each observation
                 #given sample mean
                 @F.udf(returnType=FloatType())
                 def tDistProbability(observedValue):
-                    t = (observedValue - sampleMean) / standardError
-                    p = stats.t.sf(abs(t), degreesOfFreedom)
-                    return p
-                data = data.withColumn(f"{col}_pvalue")
+                    if observedValue is not None:
+                        observedValue = float(observedValue)
+                        #t = (observedValue - sampleMean) / standardError
+                        # Using the standard error didn't work because I got all
+                        # zeros, or values very close to it.
+                        # I need the p-values to sum to 1, so
+                        # I'm going to find the proportion of the sum of all the p
+                        # values each p-value I got is. This will make values
+                        # which are more extreme proportionately more surprising than values
+                        # which are less extreme.
 
+                        t = (observedValue - sampleMean) / sampleStdDev
+                        p = float(stats.t.sf(abs(t), degreesOfFreedom))
+                        return p
+                    else:
+                        return None
+
+                self.data = self.data.withColumn(f"{col}_pvalue", tDistProbability(col))
+                columnSum = self.data.select(col).agg(F.sum(col)).collect()[0][0]
+                @F.udf(returnType=FloatType())
+                def proportionOfTotal(observedValue):
+                    if observedValue is not None:
+                        return observedValue/columnSum
+                    else:
+                        return None
+                self.data = self.data.withColumn(f"{col}_entro_p", proportionOfTotal(f"{col}_pvalue"))
+                #Ha! Ya Get it?! Entro "p"?
+                # No? Okay.
 
     def getEntropyDiscrete(self, data: pyspark.sql.DataFrame, varlist: List[str]):
         pass
--- code/main.py
+++ code/main.py
@@ -2,7 +2,7 @@ from cleanDataFrame import CleanDataFrame
 from decisionTree import DecisionTree
 from pyspark.sql import SparkSession
 from pyspark.sql import functions as F
-from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.types import StringType
 import re
 
 #set up spark
@@ -22,5 +22,7 @@ if __name__ == "__main__":
     cleanChess = CleanDataFrame("../data/chess.csv")
     chessDf = cleanChess.data
     chessTree = DecisionTree(chessDf)
-    chessTree.getEntropyContinuous(data = chessTree.data, varlist = [col for col in chessTree.data.columns])
-    chessTree.data.show()
+    chessTree.getEntropyContinuous(data = chessTree.data, varlist = ["white_rating"])
+    chessTree.data.select("white_rating_pvalue").show()
+    chessTree.data.select("white_rating_entro_p").show()
+    chessTree.data.select("white_rating_entro_p").agg(F.sum("white_rating_entro_p")).show()
