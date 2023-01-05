99444b14e1c42cb3867f5af57bd4d62ba5a7e717
Kathryn <kpanger88@gmail.com>
2023-01-04 19:10:21 -0600

    same as last

--- polarsCode/selector.py
+++ polarsCode/selector.py
@@ -1,13 +1,21 @@
 import polars as pl
+from scipy import stats
+
 class Selector():
     def __init__(self, data: pl.DataFrame):
         self.data = data
+        self.n = self.data.select(pl.count())[0,0]
 
     def getEntropyContinuousT(self, columnName: str):
         variable = self.data.select(pl.col(columnName))
         sampleMean = variable.mean()[0,0]
-        sampleStandardDev = variable.std()[0,0]
-        return sampleStandardDev
+        sampleStdDev = variable.std()[0,0]
+        standardError = sampleStdDev/self.n**(1/2)
+        degreesOfFreedom = self.n - 1
+        tValues = variable.git apply(lambda x: (x - sampleMean) / standardError)
+        pValues = tValues.apply(lambda x: float(stats.t.sf(abs(x), degreesOfFreedom)))
+        return pValues
+
     def getEntropyContinuousBins(self):
         pass
     def getEntropyDiscreet(self):
