fd908fb46f10358b9a4e53708f9537625a2f3097
KATHRYN PANGER <62573980+KathrynPanger@users.noreply.github.com>
2023-01-06 16:55:41 -0600

    done with function

--- polarsCode/selector.py
+++ polarsCode/selector.py
@@ -10,24 +10,19 @@ class Selector():
         self.n = self.data.select(pl.count())[0,0]
 
     def getEntropyContinuousT(self, columnName: str):
-        variable = self.data.select(pl.col(columnName))
-        sampleMean = variable.mean()[0,0]
-        sampleStdDev = variable.std()[0, 0]
-        standardError = sampleStdDev/self.n**(1/2)
+        originalVariable = self.data.select(pl.col(columnName))
+        sampleMean = originalVariable.mean()[0,0]
+        sampleStdDev = originalVariable.std()[0, 0]
         degreesOfFreedom = self.n - 1
+        # standardError = sampleStdDev / self.n ** (1 / 2)
         # tValues = variable.with_column(((pl.col(columnName) - sampleMean) / standardError).alias("t_values"))
-        tValues = variable.with_column(((pl.col(columnName) - sampleMean) / sampleStdDev).alias("t_values"))
+        tValues = originalVariable.with_column(((pl.col(columnName) - sampleMean) / sampleStdDev).alias("t_values"))
         pValues = tValues.with_column(pl.col("t_values").apply(lambda x: stats.t.sf(x, degreesOfFreedom)).alias("p_values"))
         pSum = pValues.sum()[0,0]
         proportionOfTotal = pValues.with_column(pl.col("p_values").apply(lambda x: x / pSum).alias("proportion_prob"))
         surprise = proportionOfTotal.with_column(pl.col("proportion_prob").apply(lambda x: math.log(1/x, 2)).alias("surprise"))
-
-        # tValues = (variable - sampleMean) / sampleStdDev
-        # pValues = tValues.apply(lambda x: [float(stats.t.sf(abs(item), degreesOfFreedom)) for item in x])
-        # pValues = variable.apply(lambda x: [float(stats.t.sf(abs((item - sampleMean) / sampleStdDev), degreesOfFreedom)) for item in x])
-        return surprise
-
-
+        entropy = surprise.sum()[0,0]
+        return entropy
 
     def getEntropyContinuousBins(self):
         pass
