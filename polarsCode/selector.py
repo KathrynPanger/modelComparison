b61962db8a9681dcc75e35c03683425d1f338c30
KATHRYN PANGER <62573980+KathrynPanger@users.noreply.github.com>
2023-01-06 16:47:00 -0600

    finish function

--- polarsCode/selector.py
+++ polarsCode/selector.py
@@ -3,6 +3,7 @@ import math
 import polars as pl
 from scipy import stats
 
+
 class Selector():
     def __init__(self, data: pl.DataFrame):
         self.data = data
@@ -14,15 +15,17 @@ class Selector():
         sampleStdDev = variable.std()[0, 0]
         standardError = sampleStdDev/self.n**(1/2)
         degreesOfFreedom = self.n - 1
+        # tValues = variable.with_column(((pl.col(columnName) - sampleMean) / standardError).alias("t_values"))
         tValues = variable.with_column(((pl.col(columnName) - sampleMean) / sampleStdDev).alias("t_values"))
-        p = tValues.with_column(pl.col("t_values").apply(lambda x: stats.t.sf(x, degreesOfFreedom)).alias("p_values"))
-        #proportionOfTotal = tValues / tValues.sum()[0,0]
-        #surprise = proportionOfTotal.apply(lambda x: [math.log(1/item, 2) for item in x])
+        pValues = tValues.with_column(pl.col("t_values").apply(lambda x: stats.t.sf(x, degreesOfFreedom)).alias("p_values"))
+        pSum = pValues.sum()[0,0]
+        proportionOfTotal = pValues.with_column(pl.col("p_values").apply(lambda x: x / pSum).alias("proportion_prob"))
+        surprise = proportionOfTotal.with_column(pl.col("proportion_prob").apply(lambda x: math.log(1/x, 2)).alias("surprise"))
 
         # tValues = (variable - sampleMean) / sampleStdDev
         # pValues = tValues.apply(lambda x: [float(stats.t.sf(abs(item), degreesOfFreedom)) for item in x])
         # pValues = variable.apply(lambda x: [float(stats.t.sf(abs((item - sampleMean) / sampleStdDev), degreesOfFreedom)) for item in x])
-        return p
+        return surprise
 
 
 
