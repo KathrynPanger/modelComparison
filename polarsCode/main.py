2b77708b39e0cfb5e166c95df373fc168cc6ceff
KATHRYN PANGER <62573980+KathrynPanger@users.noreply.github.com>
2023-01-07 13:08:02 -0600

    beginning new function dev

--- polarsCode/main.py
+++ polarsCode/main.py
@@ -3,5 +3,5 @@ from selector import Selector
 
 chessData = pl.read_csv("../data/chess.csv")
 selector = Selector(chessData)
-test = selector.getEntropyContinuousT("black_rating")
+test = selector.getEntropyDiscreet("black_rating")
 print(test)
